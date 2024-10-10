3e0861f50c6c246ce69935ab12c3aea969e659ec
kikialifaa <ALIFAHAZZ06@GMAIL.COM>
2024-10-11 04:05:46 +0700

    Add files via upload

---
+++ device_desktop/kuis_pg_hasil_kuis_pg.js
@@ -0,0 +1,24 @@
+DesktopResponsive={"1009":{
+"pageLayer":{"w":1009,"h":662,"bgColor":"#333333","bgImage":"images/background-1009.png","bgSize":"1009px 4px","bgRepeat":"repeat-y"}
+,
+"button136":{"x":967,"y":13,"w":28.000000,"h":28.000000,"stylemods":[{"sel":"div.button136Text","decl":" { position:fixed; left:2px; top:2px; width:23px; height:23px;}"},{"sel":"span.button136Text","decl":" { display:table-cell; position:relative; width:23px; height:23px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U9tCF8gAACAWw0MXAAB3XeTugAAAABJRU5ErkJggg==" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U9tCF8gAACAWw0MXAAB3XeTugAAAABJRU5ErkJggg==" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U9tCF8gAACAWw0MXAAB3XeTugAAAABJRU5ErkJggg==" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U9tCF8gAACAWw0MXAAB3XeTugAAAABJRU5ErkJggg=="  ,"fd": "images/exit-normal.png" ,"fdO": "images/exit-over.png" ,"fdD": "images/exit-click.png" ,"fdDi": "images/exit-normal.png" ,"p": "M 0.000000 0.000000 L 27.000000 0.000000 L 27.000000 27.000000 L 0.000000 27.000000 L 0.000000 0.000000 z","i":"images/button136.png","irol":"images/button136_over.png","ion":"images/button136_down.png","idis":"images/button136_disabled.png"}
+,
+"audio8053":{"x":32,"y":7,"w":30,"h":30,"bOffBottom":0}
+,
+"button8003":{"x":959,"y":617,"w":24.000000,"h":41.000000,"stylemods":[{"sel":"div.button8003Text","decl":" { position:fixed; left:2px; top:2px; width:19px; height:36px;}"},{"sel":"span.button8003Text","decl":" { display:table-cell; position:relative; width:19px; height:36px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":1  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAApCAYAAAAvUenwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U9tCy8gAAAA4KUGD4kAAdbUq7cAAAAASUVORK5CYII=" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAApCAYAAAAvUenwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U9tCy8gAAAA4KUGD4kAAdbUq7cAAAAASUVORK5CYII=" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAApCAYAAAAvUenwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U9tCy8gAAAA4KUGD4kAAdbUq7cAAAAASUVORK5CYII=" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAApCAYAAAAvUenwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U9tCy8gAAAA4KUGD4kAAdbUq7cAAAAASUVORK5CYII="  ,"fd": "images/next-normal.png" ,"fdO": "images/next-over.png" ,"fdD": "images/next-click.png" ,"fdDi": "images/next-normal.png" ,"p": "M 0.000000 0.000000 L 23.000000 0.000000 L 23.000000 40.000000 L 0.000000 40.000000 L 0.000000 0.000000 z","i":"images/button8003.png","irol":"images/button8003_over.png","ion":"images/button8003_down.png","idis":"images/button8003_disabled.png"}
+,
+"image154":{"x":0,"y":0,"w":1009,"h":88,"bOffBottom":0,"i":"images/header.png"}
+,
+"image156":{"x":0,"y":615,"w":1009,"h":47,"bOffBottom":1,"i":"images/footer.png"}
+,
+"image6286":{"x":0,"y":48,"w":1009,"h":567,"bOffBottom":0,"i":"images/math_quiz_bee_presentation_in_colorful_comic_illustrative_style_13_.png"}
+,
+"text8066":{"x":354,"y":242,"w":279,"h":200,"txtscale":100,"bOffBottom":0}
+,
+"RCDResetQuestion":function(){
+try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)console.log(e.message);}
+}
+,
+"RCDResultResize":function(){}
+,"preload":['images/next-normal.png','images/next-click.png','images/next-over.png','images/exit-normal.png','images/exit-click.png','images/exit-over.png','images/header.png','images/footer.png','images/background-1009.png','images/math_quiz_bee_presentation_in_colorful_comic_illustrative_style_13_.png','images/button136.png','images/button136_over.png','images/button136_down.png','images/button136_disabled.png','images/button8003.png','images/button8003_over.png','images/button8003_down.png','images/button8003_disabled.png']
+}}
